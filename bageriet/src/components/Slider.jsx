7fe7f3ebf47708380731c99c644f9713ce04b232
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-11-27 12:17:22 +0100

    ierah

--- bageriet/src/components/Slider.jsx
+++ bageriet/src/components/Slider.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import slide1 from "../assets/images/slide1.jpg";
 import slide2 from "../assets/images/slide2.jpg";
 import slide3 from "../assets/images/slide3.jpg";
@@ -6,15 +7,26 @@ import style from "../styling/slider.module.scss"
 
 export function Slider() {
   const sliderArray = [slide1, slide2, slide3];
+  const [slideIndex, setSlideIndex] = useState(0)
+
+  const nextSlide = () => {
+    setSlideIndex(slideIndex +1)
+  } 
+
+  const previousSlide = () => {
+    setSlideIndex(slideIndex -1)
+  }
+
+  console.log("SlideIndex", slideIndex)
 
   return (
     <header className={style.slider}>
       <div>
-        <button>Venstre</button>
+        <button onClick={()=> previousSlide()}>Venstre</button>
         <h1>Tekst her</h1>
-        <button>Højre</button>
+        <button onClick={()=> nextSlide()}>Højre</button>
       </div>
-      <img src={sliderArray[0]} alt="" />
+      <img src={sliderArray[slideIndex]} alt="" />
     </header>
   );
 }
