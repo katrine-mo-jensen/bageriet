8199756aa762572cef4d50524a56224fd45607cf
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-11-27 12:58:01 +0100

    slider dots

--- bageriet/src/components/Slider.jsx
+++ bageriet/src/components/Slider.jsx
@@ -3,30 +3,56 @@ import slide1 from "../assets/images/slide1.jpg";
 import slide2 from "../assets/images/slide2.jpg";
 import slide3 from "../assets/images/slide3.jpg";
 
-import style from "../styling/slider.module.scss"
+import style from "../styling/slider.module.scss";
 
 export function Slider() {
   const sliderArray = [slide1, slide2, slide3];
-  const [slideIndex, setSlideIndex] = useState(0)
+  const [slideIndex, setSlideIndex] = useState(0);
 
   const nextSlide = () => {
-    setSlideIndex(slideIndex +1)
-  } 
+    if (slideIndex < sliderArray.length - 1) {
+      setSlideIndex((prev) => prev + 1);
+    } else {
+      setSlideIndex(0);
+    }
+  };
 
   const previousSlide = () => {
-    setSlideIndex(slideIndex -1)
-  }
+    if (slideIndex === 0) {
+      setSlideIndex(sliderArray.length - 1);
+    } else {
+      setSlideIndex(slideIndex - 1);
+    }
+  };
 
-  console.log("SlideIndex", slideIndex)
+  const handleSliderNav = (index) => {
+    setSlideIndex(index);
+  };
+
+  console.log("SlideIndex", slideIndex);
 
   return (
     <header className={style.slider}>
       <div>
-        <button onClick={()=> previousSlide()}>Venstre</button>
+        <button onClick={() => previousSlide()}>Venstre</button>
         <h1>Tekst her</h1>
-        <button onClick={()=> nextSlide()}>Højre</button>
+        <button onClick={() => nextSlide()}>Højre</button>
       </div>
       <img src={sliderArray[slideIndex]} alt="" />
+      <section>
+        {sliderArray.map((item, index) => {
+          return (
+            <span
+              style={
+                slideIndex === index
+                  ? { backgroundColor: "rgb(122, 122, 122)" }
+                  : null
+              }
+              onClick={() => handleSliderNav(index)}
+            />
+          );
+        })}
+      </section>
     </header>
   );
 }
