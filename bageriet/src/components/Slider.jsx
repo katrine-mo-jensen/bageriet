9bdd25b6a31b82e9213a4b50f365bcab4fe93ec6
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-11-27 12:09:21 +0100

    header styling

--- bageriet/src/components/Slider.jsx
+++ bageriet/src/components/Slider.jsx
@@ -2,11 +2,13 @@ import slide1 from "../assets/images/slide1.jpg";
 import slide2 from "../assets/images/slide2.jpg";
 import slide3 from "../assets/images/slide3.jpg";
 
+import style from "../styling/slider.module.scss"
+
 export function Slider() {
   const sliderArray = [slide1, slide2, slide3];
 
   return (
-    <header>
+    <header className={style.slider}>
       <div>
         <button>Venstre</button>
         <h1>Tekst her</h1>
