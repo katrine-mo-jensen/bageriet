0a42a5aecb2241f54c0ed3e43dac8d5ef67d06f6
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-11-27 13:47:29 +0100

    nav and header close to finished
    Minor details missing

--- bageriet/src/components/Slider.jsx
+++ bageriet/src/components/Slider.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import slide1 from "../assets/images/slide1.jpg";
 import slide2 from "../assets/images/slide2.jpg";
 import slide3 from "../assets/images/slide3.jpg";
+import arrow from "../assets/images/chevron.png"
 
 import style from "../styling/slider.module.scss";
 
@@ -34,18 +35,22 @@ export function Slider() {
   return (
     <header className={style.slider}>
       <div>
-        <button onClick={() => previousSlide()}>Venstre</button>
-        <h1>Tekst her</h1>
-        <button onClick={() => nextSlide()}>Højre</button>
+        <button onClick={() => previousSlide()}>
+            <img className={style.leftArrow} src={arrow} alt="left pointing arrow" />
+        </button>
+        <h1>Vi elsker at lave brød</h1>
+        <button onClick={() => nextSlide()}>
+        <img className={style.rightArrow} src={arrow} alt="right pointing arrow" />
+        </button>
       </div>
       <img src={sliderArray[slideIndex]} alt="" />
       <section>
         {sliderArray.map((item, index) => {
           return (
-            <span
+            <span key={index}
               style={
                 slideIndex === index
-                  ? { backgroundColor: "rgb(122, 122, 122)" }
+                  ? { backgroundColor: "rgb(129,120,113)" }
                   : null
               }
               onClick={() => handleSliderNav(index)}
