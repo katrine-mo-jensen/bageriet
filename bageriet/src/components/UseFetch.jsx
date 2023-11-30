e7bb28d8e90a4a47eddf71956a86aa93da4d3d60
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-11-30 09:26:10 +0100

    Categories works

--- bageriet/src/components/UseFetch.jsx
+++ bageriet/src/components/UseFetch.jsx
@@ -2,13 +2,26 @@ import { useEffect, useState } from "react";
 
 export function useFetch(url) {
   const [data, setData] = useState();
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     fetch(url)
-      .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        setError(error); 
+      });
   }, [url]);
 
-  return (
-    {data}
-  )
+  return {
+    data,
+    error
+  };
 }
--- bageriet/src/pages/ProductPage.jsx
+++ bageriet/src/pages/ProductPage.jsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from "react";
+import { useFetch } from "../components/UseFetch";
+
+
 export function ProductPage() {
-    // Fetch categories 
-    //Display categories 
-    //Fetch what is in the categories
-    //Display what is in the categories 
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("1");
+  const [selectedCategoryData, setSelectedCategoryData] = useState(null);
+
+  const url = `https://api.mediehuset.net/bakeonline/categories/${selectedCategory}`;
+  const { data: categoriesData } = useFetch(url);
+  console.log("Products in the category", categoriesData);
+
+  useEffect(() => {
+    const urlCategories = "https://api.mediehuset.net/bakeonline/categories";
+    fetch(urlCategories)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && data.status === "Ok") {
+          setCategories(data.items); // Set categories to the 'items' array from the response
+          console.log("Categories", data.items); // Log the fetched categories
+        } else {
+          console.error("Error fetching categories:", data.error);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Fetch data for the selected category
+    const fetchCategoryData = async () => {
+      const response = await fetch(
+        `https://api.mediehuset.net/bakeonline/categories/${selectedCategory}`
+      );
+      const data = await response.json();
+      setSelectedCategoryData(data);
+      console.log("Selected Category Data", data);
+    };
+
+    fetchCategoryData();
+  }, [selectedCategory]);
+
+  const handleCategoryClick = (categoryId) => {
+    setSelectedCategory(categoryId);
+  };
 
   return (
     <section>
@@ -14,8 +56,27 @@ export function ProductPage() {
       </p>
       <section>
         <aside>
-
+          <ul>
+            {categories.map((item) => (
+              <li
+                key={item.id}
+                onClick={() => handleCategoryClick(item.id)}
+                style={{ cursor: "pointer" }}
+              >
+                {item.title}
+              </li>
+            ))}
+          </ul>
         </aside>
+        <section>
+          {selectedCategoryData && (
+            <section>
+              <h3>{selectedCategoryData.title}</h3>
+              <p>ID: {selectedCategoryData.id}</p>
+              {/* Display other relevant details here */}
+            </section>
+          )}
+        </section>
       </section>
     </section>
   );
