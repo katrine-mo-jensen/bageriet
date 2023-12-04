9286e520dd74a74beb2e6e3e6649373e0ed3d488
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-12-04 09:36:32 +0100

    Login & contact, damn close

--- bageriet/src/pages/ContactPage.jsx
+++ bageriet/src/pages/ContactPage.jsx
@@ -1,81 +1,47 @@
-import React, { useState } from "react";
-// https://herotofu.com/solutions/guides/react-contact-form
-// Used this for contact form
-
-const FORM_ENDPOINT = "https://herotofu.com/start"; // TODO - update to the correct endpoint
-
-import map from "../assets/images/dolly.png"
+import map from "../assets/images/dolly.png";
+import style from "../styling/form.module.scss";
 
 export const ContactPage = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const inputs = e.target.elements;
-    const data = {};
-
-    for (let i = 0; i < inputs.length; i++) {
-      if (inputs[i].name) {
-        data[inputs[i].name] = inputs[i].value;
-      }
-    }
-
-    fetch(FORM_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Form response was not ok');
-        }
-
-        setSubmitted(true);
-      })
-      .catch((err) => {
-        // Submit the form manually
-        e.target.submit();
-      });
-  };
-
-  if (submitted) {
-    return (
-      <>
-        <h2>Thank you!</h2>
-        <div>We'll be in touch soon.</div>
-      </>
-    );
-  }
-
   return (
-    <section>
-    <form
-      action={FORM_ENDPOINT}
-      onSubmit={handleSubmit}
-      method="POST"
-    >
-      <div>
-        <input type="text" placeholder="Dit navn..." name="name" required />
-      </div>
-      <div>
-        <input type="email" placeholder="Din email..." name="email" required />
-      </div>
-      <div>
-        <textarea placeholder="Din besked..." name="message" required />
-      </div>
-      <div>
-        <button type="submit">Send</button>
-      </div>
-    </form>
-    <section>
-        <p><span>adresse:</span> Øster uttrupvej 1, 9200 aalborg</p>
-        <p><span>telefon:</span> +45 25 26 95 40</p>
-        <img src={map} alt="" />
+    <section className={style.formWrapper}>
+      <section>
+        <h2>Kontakt os</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt cumque
+          minus tenetur aliquid ab accusamus quos ullam libero atque, <br /> temporibus
+          consequuntur dolore in voluptate laboriosam et eius vel illo ratione.
+        </p>
+      </section>
+    <section className={style.contactForm}>
+      <form>
+        <div>
+          <input type="text" placeholder="Dit navn..." name="name" required />
+        </div>
+        <div>
+          <input
+            type="email"
+            placeholder="Din email..."
+            name="email"
+            required
+          />
+        </div>
+        <div>
+          <textarea placeholder="Din besked..." name="message" required />
+        </div>
+        <div className={style.btnContainer}>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+      <section className={style.location}>
+        <p>
+          <span>adresse:</span> Øster uttrupvej 1, 9200 aalborg
+        </p>
+        <p>
+          <span>telefon:</span> +45 25 26 95 40
+        </p>
+        <img src={map} alt="Map" />
+      </section>
     </section>
     </section>
   );
 };
-
--- bageriet/src/pages/LoginPage.jsx
+++ bageriet/src/pages/LoginPage.jsx
@@ -1,3 +1,39 @@
+import style from "../styling/form.module.scss";
+
 export const LoginPage = () => {
-  return <section>LoginPage</section>;
+  return (
+    <section className={style.formWrapper}>
+      <h2>Log ind</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolores
+        earum atque aut voluptate explicabo quaerat quos unde cupiditate <br />
+        laudantium itaque aliquam suscipit inventore facere error ipsa incidunt,
+        exercitationem numquam.
+      </p>
+      <section className={style.loginForm}>
+        <form>
+          <div>
+            <input
+              type="text"
+              placeholder="Brugernavn..."
+              name="username"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password..."
+              name="password"
+              required
+            />
+          </div>
+
+          <div className={style.btnContainer}>
+            <button type="submit">Log ind</button>
+          </div>
+        </form>
+      </section>
+    </section>
+  );
 };
